4fff2fabe03140d228ed1b13dc5a4868a0fc91c5
codwerk <louis_widdy@live.fr>
2020-09-02 23:21:39 +0200

    intégration html/css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template lang="pug">
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  #app
+    nav
+      .nav-container
+        #logo
+          router-link(to="/")
+            img(src="./assets/logo.png", alt="Vue logo")
+
+        ul#container-list
+          li 
+            router-link(to="/user-settings")
+              i.fas.fa-user
+          li 
+            router-link(to="/wish-list")
+              i.fas.fa-heart
+          li 
+            router-link(to="/shopping-cart")
+              i.fas.fa-shopping-cart
+
+    transition(name="fade" mode="out-in")
+      router-view
+
 </template>
 
 <script>
@@ -17,12 +34,67 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  @import "assets/scss/index.scss";
+  margin: 0;
+
+    #app {
+      
+
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+
+    nav {
+
+      padding: 0.8rem 2.4rem;
+      background: rgba(255, 255, 255, 0.97);
+      width: 100%;
+      z-index: 2;
+      box-shadow: 0 2px 4px -3px #41b883;
+      .nav-container {
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        max-width: 1050px;
+        margin: 0 auto;
+        #logo {
+          height: 32px;
+          width: 35px;
+        }
+
+        #container-list {
+          display: flex;
+          font-size: 24px;
+          width: 34%;
+          justify-content: space-around;
+
+          li {
+            cursor: pointer;
+
+            a {
+
+              i {
+                color: #41b883;
+              }
+            }
+          }
+        }
+      }
+    }
+
+      .fade-enter , .fade-leave-to {
+      opacity: 0;
+      transform: translateX(2em);
+    }
+    .fade-enter-active , .fade-leave-active {
+      transition: all .3s ease-in-out;
+    }
+    }
 }
+
 </style>
