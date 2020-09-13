8130fc2eee4e339b528655908b09b77176673d0e
codwerk <louis_widdy@live.fr>
2020-09-13 15:49:03 +0200

    custom style article

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -4,10 +4,16 @@
         img(:src="product.img", :alt="product.titre").card__img
         .card__data
           h1.card__title {{ product.titre }}
-          span.card__price ${{ product.price }}
+          span.card__price {{ product.price }}€
           p.card__txt {{ product.description }}
-          a(href="#").card__buy buy
-          a(href="#").card__like
+          .wrapper__btns
+            a(href="#").card__like
+              input(type="checkbox", name="checkHeart", :id="product.id")
+              label(:for="product.id")
+                i.fas.fa-heart
+            a(href="#").card__buy 
+              i.fas.fa-shopping-cart
+            
 
 </template>
 
@@ -27,7 +33,7 @@
 /*===== Couleur =====*/
 $first-color: #F2A20C;
 $white-color: #E9EAEC;
-$dark-color: #272D40;
+$dark-color: #41b883;//272D40
 $dark-color-lighten: #F2F5FF;
 
 /*===== Police et typographie =====*/
@@ -36,14 +42,82 @@ $h2-font-size: 1.25rem;
 $normal-font-size: 1rem;
 $small-font-size: .875rem;
 
+a {
+  text-decoration: none;
+}
+
 .card {
+  position: relative;
   width: 320px;
   height: 400px;
-  background-color: $dark-color;
-  border-radius: 1rem;
-  padding: 4rem 2rem 0;
+  //background-color: $dark-color;
+  padding: 8rem 2rem 0;
   color: $white-color;
-  overflow-y: hidden;
+  overflow: hidden;
+  cursor: pointer;
+  margin: 0px 10px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    overflow: hidden;
+    width: 150%;
+    height: 70%;
+    top: -160%;
+    bottom: -20%;
+    left: -20%;
+    transform: rotate(5deg);
+    background-color: $dark-color;
+    z-index: -1;
+    transition: .5s;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    overflow: hidden;
+    width: 150%;
+    height: 70%;
+    bottom: 10%;
+    left: -20%;
+    transform: rotate(5deg);
+    background-color: $dark-color;
+    z-index: -1;
+    transition: .5s;
+  }
+
+
+   &:hover {
+     
+    .card__img {
+      transform: translate(-1.5rem, -8.5rem) rotate(-20deg);
+    }
+
+    .card__data {
+      transform: translateY(2.8rem);
+      
+      .card__title {
+        transform: translateX(-2.8rem);
+      }
+
+      .card__price {
+        transform: translateX(-6.8rem);
+      }
+
+      .card__txt, .wrapper__btns a {
+        transition-delay: .2s;
+        opacity: 1;
+      }
+    }
+    
+    &::before {
+      top: 10%;
+    }
+
+    &::after {
+      bottom: -200%;
+    }
+  }
 
   .card__img{
     position: absolute;
@@ -52,12 +126,12 @@ $small-font-size: .875rem;
   }
 
   .card__data {
-    transform: translateY(13.2rem);
+    transform: translateY(10.2rem);
     text-align: center;
 
     .card__title {
       font-size: $h1-font-size;
-      color: $first-color;
+      color: #272D40;
       margin-bottom: .5rem;
     }
 
@@ -74,17 +148,61 @@ $small-font-size: .875rem;
       margin-bottom: 1.25rem;
       opacity: 0;
     }
-    .card__buy {
-      display: block;
-      width: max-content;
-      padding: 1.125rem 2rem;
-      background: #F2A20C;
-      color: #E9EAEC;
-      border-radius: .5rem;
-      font-weight: 600;
-      transition: .2s;
-      opacity: 0;
+    
+    .wrapper__btns {
+      display: flex;
+      justify-content: space-around;
+      a {
+        display: block;
+        width: max-content;
+        padding: 1.125rem 2rem;
+        color: #272D40;
+        border-radius: .5rem;
+        font-weight: 600;
+        transition: .2s;
+        opacity: 0;
+        font-size: 2rem;
+        
+
+        input {
+          display: none;
+
+          &:checked + label i {
+            color: rgb(251, 38, 38);
+          }
+        }
+
+        label {
+          i {
+            transition: .5s;
+            cursor: pointer;
+
+            &:hover {
+              animation: scaler 0.8s infinite linear;
+            }
+          }
+
+          .fa-heart:hover {
+            color: rgba(251, 38, 38, 0.5);
+          }
+        }
+
+
+      }
     }
   }
+}
+
+@keyframes scaler {
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.card__img, .card__data, .card__title, .card__price, .card__txt {
+    transition: .5s;
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -3,6 +3,15 @@
     .wrapper
       .wrapper__title
         h3.title Article
+
+      //Search display
+
+      .form__group.field
+        input(type="text", v-model="searchVal" class="form__input")
+        label Search Shoes
+        span
+      
+      //Cards display
       .container__article
         Article(v-for="product in products" :key="product.id" :product="product")
 </template>
@@ -23,24 +32,31 @@ import Article from '../components/Article'
            titre:'Nike Air Jordan',
            description: 'Nike Air Jordan Footwear basketball sneakers.',
            price: 99,
-           img: require('../assets/img/img5.png')
+           img: require('../assets/img/img3.png')
          },
          {
            id:2,
            titre:'Nike Air Jordan',
            description: 'Nike Air Jordan Footwear basketball sneakers.',
            price: 40,
-           img:require('../assets/img/img5.png')
+           img:require('../assets/img/img2.png')
          },
          {
            id:3,
            titre:'Nike Air Jordan',
            description: 'Nike Air Jordan Footwear basketball sneakers.',
            price: 60,
-           img:require('../assets/img/img5.png')
-         }
-         ]
+           img:require('../assets/img/img1.png')
+         }         
+         ],
+         searchVal: ''
      }
+   },
+   computed: {
+
+   },
+   methods: {
+     
    }
   }
 </script>
